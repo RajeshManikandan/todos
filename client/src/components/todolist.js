8733b05a031f2936448e3631cc96c2838e7cbc49
Rajesh Sambasivam <rajbca00@gmail.com>
2018-11-20 06:35:41 +0530

    save

--- client/src/components/todolist.js
+++ client/src/components/todolist.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ListElement from './listelement';
 import axios from 'axios';
+import '../styles/todolist.css';
 class TodoList extends Component {
     state = {
         list: []
@@ -24,13 +25,15 @@ class TodoList extends Component {
     addTodo = e => {
         const newTodo = this._inputElement.value;
         this._inputElement.value = '';
-        axios
-            .post('/api/todo', { name: newTodo })
-            .then(todo => {
-                console.log(todo);
-                this.updateList();
-            })
-            .catch(err => console.log(err));
+        if (newTodo !== '') {
+            axios
+                .post('/api/todo', { name: newTodo })
+                .then(todo => {
+                    console.log(todo);
+                    this.updateList();
+                })
+                .catch(err => console.log(err));
+        }
 
         e.preventDefault();
     };
@@ -49,15 +52,19 @@ class TodoList extends Component {
         const { list } = this.state;
         console.log(list);
         return (
-            <div>
+            <div className="todo">
                 <h2>Todo App</h2>
                 <form onSubmit={this.addTodo}>
-                    <input name="name" ref={a => (this._inputElement = a)} />
+                    <input name="name" placeholder="Enter Here..." ref={a => (this._inputElement = a)} />
                     <button>ADD</button>
                 </form>
-                {list.length > 0
-                    ? list.map(todo => <ListElement pid={todo._id} pname={todo.name} deletethis={this.removeTodo} />)
-                    : null}
+                <ul>
+                    {list.length > 0
+                        ? list.map(todo => (
+                              <ListElement pid={todo._id} pname={todo.name} deletethis={this.removeTodo} />
+                          ))
+                        : null}
+                </ul>
             </div>
         );
     }
