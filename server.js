c3b1b06850a719845b731e3d916a3b947d94297b
Rajesh Sambasivam <rajbca00@gmail.com>
2019-02-02 04:17:41 +0530

    all

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //listening to port
-const port = process.env.port | 80;
+const port = process.env.port || 8080;
 app.listen(port, () => console.log('server is running in port ' + port));
